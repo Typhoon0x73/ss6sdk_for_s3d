93595de73981f937ee5a1fdc23ffc7b6724bdbbf
Inoue_Satsuki <s.inoue@otwo.jp>
2023-01-11 13:09:48 +0900

    セルマップ描画のサンプルコードを追加

--- ss6sdk_for_s3d/Main.cpp
+++ ss6sdk_for_s3d/Main.cpp
@@ -1,4 +1,5 @@
 ﻿# if !defined(USE_TEST_MAIN)
+# if !defined(USE_SAMPLE_MAIN)
 
 # include <Siv3D.hpp> // OpenSiv3D v0.6.6
 
@@ -9,4 +10,5 @@ void Main()
 	}
 }
 
+# endif // !defined(USE_SAMPLE_MAIN)
 # endif // !defined(USE_TEST_MAIN)
---
+++ ss6sdk_for_s3d/SampleMain/__SampleDefine.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+# if defined(USE_SAMPLE_MAIN)
+
+#     define CELLMAP_DRAW_SAMPLE_CPP
+
+# endif // defined(USE_SAMPLE_MAIN)
