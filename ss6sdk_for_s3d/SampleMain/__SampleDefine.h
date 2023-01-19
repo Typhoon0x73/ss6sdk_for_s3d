2ad8fbe65b3363c5c9e72c4480309783d10f8830
Inoue_Satsuki <s.inoue@otwo.jp>
2023-01-19 18:53:00 +0900

    メッシュ情報からBuffer2Dを作成し描画するサンプルの追加

--- ss6sdk_for_s3d/SampleMain/__SampleDefine.h
+++ ss6sdk_for_s3d/SampleMain/__SampleDefine.h
@@ -2,6 +2,7 @@
 
 # if defined(USE_SAMPLE_MAIN)
 
-#     define CELLMAP_DRAW_SAMPLE_CPP
+//#     define CELLMAP_DRAW_SAMPLE_CPP
+#     define BUFFER2D_DRAW_SAMPLE_CPP
 
 # endif // defined(USE_SAMPLE_MAIN)
