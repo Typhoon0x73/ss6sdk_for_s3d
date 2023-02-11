ba453a0d423be567d79cf91c34407767590a4f67
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-02-11 15:44:13 +0900

    エディタパラメータ生成用フラグ引数をbool値からenumへ変更、読み込み関数をテーブル化

--- ss6sdk_for_s3d/SpriteStudio/SSProject.cpp
+++ ss6sdk_for_s3d/SpriteStudio/SSProject.cpp
@@ -4,32 +4,47 @@
 namespace s3d
 {
 
-	SSProject::SSProject(FilePathView path, bool createEditorParam)
+	SSProject::SSProject(FilePathView path, CreateEditorParam createEditorParam)
 		: SSProject()
 	{
 		load(path, createEditorParam);
 	}
 
-	bool SSProject::load(FilePathView path, bool createEditorParam)
+	bool SSProject::load(FilePathView path, CreateEditorParam createEditorParam)
 	{
 		XMLReader reader{ path };
 		if (not reader.isOpen() or reader.isNull())
 		{
 			return false;
 		}
-		m_projectDirectory = FileSystem::RelativePath(FileSystem::ParentPath(path), FileSystem::ModulePath());
-		m_projectFileName  = FileSystem::FileName(path);
+		m_projectDirectory  = FileSystem::RelativePath(FileSystem::ParentPath(path), FileSystem::ModulePath());
+		m_projectFileName   = FileSystem::FileName(path);
+		m_createEditorParam = createEditorParam;
+
+		static const HashTable<String, void (SSProject::*)(const XMLElement&)> PARSE_TABLE = {
+			{ SS_PROJECT_TAG_STRINGS[SSProjectTag_name    ], &SSProject::parseFileName },
+			{ SS_PROJECT_TAG_STRINGS[SSProjectTag_settings], &SSProject::parseSettings },
+		};
+
 		for (auto element = reader.firstChild(); element; element = element.nextSibling())
 		{
-			if (element.name().compare(SS_PROJECT_TAG_STRINGS[SSProjectTag_name]) == 0)
+			for(const auto& table : PARSE_TABLE)
+			if (table.first == element.name())
 			{
-				m_projectName = element.text();
-			}
-			else if (element.name().compare(SS_PROJECT_TAG_STRINGS[SSProjectTag_settings]) == 0)
-			{
-				m_projectSettings.load(element, createEditorParam);
+				(this->*(table.second))(element);
+				break;
 			}
 		}
 		return true;
+	}
+
+	void SSProject::parseFileName(const XMLElement& element)
+	{
+		m_projectName = element.text();
+	}
+
+	void SSProject::parseSettings(const XMLElement& element)
+	{
+		m_projectSettings.load(element, (m_createEditorParam == CreateEditorParam::Create));
 	}
 }
--- ss6sdk_for_s3d/SpriteStudio/SSProject.hpp
+++ ss6sdk_for_s3d/SpriteStudio/SSProject.hpp
@@ -9,19 +9,33 @@ namespace s3d
 	{
 	public:
 
+		/// @brief エディタ用パラメータを生成するかの列挙値
+		enum class CreateEditorParam
+		{
+			NoCreate,                 //!< 生成しない
+			Create,                   //!< 生成する
+		};
+
 		SSProject() = default;
 
 		~SSProject() = default;
 
-		explicit SSProject(FilePathView path, bool createEditorParam);
+		explicit SSProject(FilePathView path, CreateEditorParam createEditorParam);
 
-		bool load(FilePathView path, bool createEditorParam);
+		bool load(FilePathView path, CreateEditorParam createEditorParam);
 
 	private:
 
-		FilePath          m_projectDirectory{ U"" }; //!< exeからのプロジェクトのディレクトリ相対パス
-		FilePath          m_projectFileName { U"" }; //!< プロジェクトファイル名
-		String            m_projectName     { U"" }; //!< プロジェクト名
-		SSProjectSettings m_projectSettings {};      //!< プロジェクト設定
+		// -------------------------------------------------
+		// 解析用関数
+		// -------------------------------------------------
+		void parseFileName(const XMLElement& element);
+		void parseSettings(const XMLElement& element);
+
+		CreateEditorParam m_createEditorParam{};      //!< エディタ用パラメータを生成する設定
+		FilePath          m_projectDirectory { U"" }; //!< exeからのプロジェクトのディレクトリ相対パス
+		FilePath          m_projectFileName  { U"" }; //!< プロジェクトファイル名
+		String            m_projectName      { U"" }; //!< プロジェクト名
+		SSProjectSettings m_projectSettings  {};      //!< プロジェクト設定
 	};
 }
