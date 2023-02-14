a30618030ebb740052fa63e06a876db8d66c42e4
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-02-14 21:27:03 +0900

    AnimeSettingsクラスの追加

---
+++ ss6sdk_for_s3d/SpriteStudio/SSAnimeSettings.hpp
@@ -0,0 +1,40 @@
+﻿
+# pragma once
+# include <Siv3D.hpp>
+# include "SSBackgroundSettings.hpp"
+
+namespace s3d
+{
+
+	class SSAnimeSettings
+	{
+	public:
+
+		enum class SortMode
+		{
+			prio,
+			posz,
+		};
+
+		SSAnimeSettings() = default;
+
+		~SSAnimeSettings() = default;
+
+	private:
+
+		int32                m_fps        { 0 };
+		int32                m_frameCount { 0 };
+		SortMode             m_sortMode   {};
+		Size                 m_canvasSize { 0, 0 };
+		Float2               m_pivot      { 0.0f, 0.0f };
+		ColorF               m_bgColor    {};
+		int32                m_gridSize   { 0 };
+		ColorF               m_gridColor  {};
+		int32                m_ik_depth   { 0 };
+		int32                m_startFrame { 0 };
+		int32                m_endFrame   { 0 };
+		SSBackgroundSettings m_bgSettings {};
+		int32                m_outStartNum{ 0 };
+
+	};
+}
---
+++ ss6sdk_for_s3d/SpriteStudio/SSBackgroundSettings.hpp
@@ -0,0 +1,24 @@
+﻿
+# pragma once
+# include <Siv3D.hpp>
+
+namespace s3d
+{
+
+	class SSBackgroundSettings
+	{
+	public:
+
+		SSBackgroundSettings() = default;
+
+		~SSBackgroundSettings() = default;
+
+	private:
+
+		FilePath m_imagePath  { U"" };
+		int32    m_imageDisp  { 0 };
+		Point    m_imageOffset{ 0, 0 };
+		Size     m_imageCanvas{ 0, 0 };
+		Float2   m_imagePivot { 0.0f, 0.0f };
+	};
+}
