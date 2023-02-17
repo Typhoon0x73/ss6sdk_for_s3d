95f003c5a146077d518daeb4521efbef8ee986e2
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-02-17 13:42:20 +0900

    ビルドエラーの修正

--- ss6sdk_for_s3d/SpriteStudio/SSAnimeSettings.cpp
+++ ss6sdk_for_s3d/SpriteStudio/SSAnimeSettings.cpp
@@ -14,19 +14,19 @@ namespace s3d
 	{
 		m_isCreateEditorParam = createEditorParam;
 		static const HashTable<String, void(SSAnimeSettings::*)(const XMLElement&)> PARSE_TABLE = {
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_fps        ], SSAnimeSettings::parseFps                },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_frameCount ], SSAnimeSettings::parseFrameCount         },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_sortMode   ], SSAnimeSettings::parseSortMode           },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_canvasSize ], SSAnimeSettings::parseCanvasSize         },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_pivot      ], SSAnimeSettings::parsePivot              },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_bgColor    ], SSAnimeSettings::parseBackGroundColor    },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_gridSize   ], SSAnimeSettings::parseGridSize           },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_gridColor  ], SSAnimeSettings::parseGridColor          },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_ik_depth   ], SSAnimeSettings::parseIKDepth            },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_startFrame ], SSAnimeSettings::parseStartFrame         },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_endFrame   ], SSAnimeSettings::parseEndFrame           },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_bgSettings ], SSAnimeSettings::parseBackgroundSettings },
-			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_outStartNum], SSAnimeSettings::parseOutStartNum        },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_fps        ], &SSAnimeSettings::parseFps                },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_frameCount ], &SSAnimeSettings::parseFrameCount         },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_sortMode   ], &SSAnimeSettings::parseSortMode           },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_canvasSize ], &SSAnimeSettings::parseCanvasSize         },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_pivot      ], &SSAnimeSettings::parsePivot              },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_bgColor    ], &SSAnimeSettings::parseBackGroundColor    },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_gridSize   ], &SSAnimeSettings::parseGridSize           },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_gridColor  ], &SSAnimeSettings::parseGridColor          },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_ik_depth   ], &SSAnimeSettings::parseIKDepth            },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_startFrame ], &SSAnimeSettings::parseStartFrame         },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_endFrame   ], &SSAnimeSettings::parseEndFrame           },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_bgSettings ], &SSAnimeSettings::parseBackgroundSettings },
+			{ SS_ANIME_SETTINGS_TAG_STRINGS[SSAnimeSettingsTag_outStartNum], &SSAnimeSettings::parseOutStartNum        },
 		};
 		for (auto element = animeSettings.firstChild(); element; element = element.nextSibling())
 		{
