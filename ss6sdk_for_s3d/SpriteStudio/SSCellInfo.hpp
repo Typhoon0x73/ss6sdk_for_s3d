0bb9706dcd1d70db68af3ba3b0b953a119b3acd2
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-03-03 18:41:29 +0900

    セル情報のメンバを追加

--- ss6sdk_for_s3d/SpriteStudio/SSCellInfo.hpp
+++ ss6sdk_for_s3d/SpriteStudio/SSCellInfo.hpp
@@ -17,5 +17,19 @@ namespace s3d
 		bool load(const XMLElement& cell, bool createEditorParam);
 
 	private:
+
+		String               m_name         { U"" };
+		Point                m_pos          { 0, 0 };
+		Size                 m_size         { 0, 0 };
+		Float2               m_pivot        { 0.0f, 0.0f };
+		int32_t              m_rotated      { 0 };
+		String               m_orgImageName { U"" };
+		bool                 m_posStable    { false };
+		bool                 m_ismesh       { false };
+		String               m_divtype      { U"" };
+		Array<Point>         m_innerPoint   {};
+		Array<Point>         m_outerPoint   {};
+		Array<Point>         m_meshPointList{};
+		Array<TriangleIndex> m_meshTriList  {};
 	};
 }
