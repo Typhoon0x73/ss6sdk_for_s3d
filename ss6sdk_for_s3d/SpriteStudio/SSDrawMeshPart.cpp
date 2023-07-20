6fd10b6f874d9d41a1514bb37d5cb8bd0543d226
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-07-20 16:15:24 +0900

    メッシュパーツ用の描画クラスの準備

---
+++ ss6sdk_for_s3d/SpriteStudio/SSDrawMeshPart.cpp
@@ -0,0 +1,22 @@
+﻿
+#include "SSDrawMeshPart.hpp"
+
+namespace sssdk
+{
+	SSDrawMeshPart::SSDrawMeshPart(const SSAnimationPart* setup, ISSCellmaps* cellmaps)
+		: SSDrawCellPart{ setup, cellmaps }
+		, m_pTargetTexture{ nullptr }
+		, m_pMesh2D{ nullptr }
+	{
+		// TODO
+	}
+
+	SSDrawMeshPart::~SSDrawMeshPart()
+	{
+	}
+
+	void SSDrawMeshPart::draw(const Vec2& canvasOffset) const
+	{
+		// TODO
+	}
+}
---
+++ ss6sdk_for_s3d/SpriteStudio/SSDrawMeshPart.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+#ifndef SS_DRAW_MESH_PART_H_
+#define SS_DRAW_MESH_PART_H_
+
+#include "SSDrawCellPart.hpp"
+
+namespace sssdk
+{
+	class SSDrawMeshPart : public SSDrawCellPart
+	{
+	public:
+
+		explicit SSDrawMeshPart(const SSAnimationPart* setup, ISSCellmaps* cellmaps);
+		virtual ~SSDrawMeshPart();
+
+		virtual void draw(const Vec2& canvasOffset) const override;
+
+	protected:
+
+		std::unique_ptr<MSRenderTexture> m_pTargetTexture;
+		std::unique_ptr<Buffer2D> m_pMesh2D;
+
+	};
+}
+
+#endif // !SS_DRAW_MESH_PART_H_
