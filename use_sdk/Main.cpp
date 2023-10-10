042f199ddc7484a8e1ced4a33ffb900f8a2cf431
Typhoon0x73 <akasugitasuisei@gmail.com>
2023-10-10 15:02:24 +0900

    Update Main.cpp

--- use_sdk/Main.cpp
+++ use_sdk/Main.cpp
@@ -2,9 +2,19 @@
 # if !defined(USE_SAMPLE_MAIN)
 
 # include <Siv3D.hpp> // OpenSiv3D v0.6.10
+# include "ThirdParty/sssdk/Common/Loader/ssloader_sspj.h"
 
 void Main()
 {
+	Console.open();
+
+	std::unique_ptr<spritestudio6::SsProject> pSsProject = nullptr;
+	pSsProject.reset(spritestudio6::ssloader_sspj::Load("ss6sample/Ringo/Ringo.sspj"));
+	if (pSsProject == nullptr)
+	{
+		return;
+	}
+
 	while (System::Update())
 	{
 	}
